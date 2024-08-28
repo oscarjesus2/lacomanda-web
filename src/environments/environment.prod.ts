edd1c2f7a1d1350c510237ec13e5801258f0f61e
oscarjesus2 <oscarjesusbautistalopez@gmail.com>
2024-08-28 10:48:17 +0200

    correcion de develop y master

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ad4c7ca3-15da-4ee8-92e0-e9bbaed2f6ca.clouding.host/api',
-  apiPrintUrl : 'https://services.jobbusiness.pe'
+  apiUrl: 'https://ad4c7ca3-15da-4ee8-92e0-e9bbaed2f6ca.clouding.host/api'
 };
