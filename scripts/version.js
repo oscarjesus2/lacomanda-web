a3f7fa2439973026334cc0d7dade353c3234ea04
osbaulo <osbaulo@uv.es>
2024-10-13 16:56:56 +0200

    reiniciando el versionamiento.

--- scripts/version.js
+++ scripts/version.js
@@ -1,4 +1,4 @@
-const { version } = require('./package.json'); // Cambia a ruta correcta
+const { version } = require('../package.json'); // Cambia a ruta correcta
 const { writeFileSync } = require('fs');
 const { join } = require('path');
 
