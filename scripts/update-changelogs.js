86cd5c10ac6da9f6f4d783e5cae0458d960dae79
osbaulo <osbaulo@uv.es>
2024-10-13 17:48:01 +0200

    Se reiniciar el versionamiento.

--- scripts/update-changelogs.js
+++ scripts/update-changelogs.js
@@ -10,11 +10,13 @@ function updateChangelog(env) {
     const envChangelogPath = path.join(__dirname, `../changelogs/${env}-changelog.md`);
     fs.appendFileSync(envChangelogPath, changelogContent); // Añadir contenido al changelog del entorno
     console.log(`Changelog actualizado para ${env}`);
+
+    fs.writeFileSync(changelogPath, ''); // Dejar el archivo vacío
+    console.log(`CHANGELOG.md limpiado.`);
 }
 
 // Verifica si se pasa el entorno como argumento
 const environment = process.argv[2]; // Recibe el entorno como argumento (development, testing, production)
-
 if (environment) {
     updateChangelog(environment);
 } else {
