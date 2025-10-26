0e4a4d250a07069e20852e989899b47913988eb9
osbaulo <osbaulo@uv.es>
2025-10-26 11:22:47 +0100

    correccion de nombre api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://production.api.lacomanda.store'
+  apiUrl: 'https://production.api.lacomanda.store/api'
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://staging.api.lacomanda.store'
+    apiUrl: 'https://staging.api.lacomanda.store/api'
   };
