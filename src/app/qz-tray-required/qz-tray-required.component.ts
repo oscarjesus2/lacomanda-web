cef88733e7a4bb3dca5c68d991591c37cb6819a1
Oscar Bautista Lopez <obautista@ccap-code.com>
2026-08-13 03:50:18 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/qz-tray-required/qz-tray-required.component.ts
+++ src/app/qz-tray-required/qz-tray-required.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./qz-tray-required.component.css']
 })
 export class QzTrayRequiredComponent {
-
+  readonly qzTrayDownloadUrl = 'https://qz.io/download/';
+  readonly certificateUrl = 'assets/signing/certificate.pem';
+  readonly manualUrl = 'assets/manuales/manual-instalacion-configuracion-qz-tray.pdf';
 }
